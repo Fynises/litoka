eb3d13aa2a526485cf66e861b01a60b7abb5d9d2
Fynises <fraser_irvine@outlook.com>
2022-10-27 21:36:16 +0100

    make simple hello world response

--- server/express.ts
+++ server/express.ts
@@ -1,5 +1,12 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
+
+app.use('/dist', express.static(path.join(process.cwd(), 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'public/index.html'));
+});
 
 export default app;
