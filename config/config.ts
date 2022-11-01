4ad3badd864c38b5ba3a394e36d79d8c0328a225
Fynises <fraser_irvine@outlook.com>
2022-11-01 16:58:21 +0000

    added api for getting websocket details

--- config/config.ts
+++ config/config.ts
@@ -1,5 +1,6 @@
 const config = {
-  port: process.env.PORT
+  port: process.env.PORT,
+  wsUrl: process.env.WS_URL
 };
 
 export default config;
--- server/express.ts
+++ server/express.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import config from '../config/config';
 
 const app = express();
 
@@ -7,6 +8,14 @@ app.use('/dist', express.static(path.join(process.cwd(), 'dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(process.cwd(), 'public/index.html'));
+});
+
+app.get('/api/getwebsocket', (req, res) => {
+  try {
+    res.json({wsUrl: config.wsUrl});
+  } catch (err) {
+    return res.status(400).json({ error: err });
+  }
 });
 
 export default app;
