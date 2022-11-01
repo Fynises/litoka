b0baab0ea695ff54e5c492db9dd1f4712b2c09c9
Fynises <fraser_irvine@outlook.com>
2022-11-01 22:46:16 +0000

    added a testing post request for reference later

--- server/express.ts
+++ server/express.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import path from 'path';
 import config from '../config/config';
+import wsMap from './websocket-map';
 
 const app = express();
+app.use(express.json());
 
 app.use('/dist', express.static(path.join(process.cwd(), 'dist')));
 
@@ -15,6 +17,19 @@ app.get('/api/getwebsocket', (req, res) => {
     res.json({wsUrl: config.wsUrl});
   } catch (err) {
     return res.status(400).json({ error: err });
+  }
+});
+
+//for reference later
+app.post('/api/testwebsocket', (req, res) => {
+  const target: string = req.body.target;
+  if (wsMap.get(target) !== undefined) {
+    wsMap.get(target).forEach((i) => {
+      i.wsObj.send('message from server');
+    });
+    return res.status(200).json({ message: 'successfully sent websocket messages'});
+  } else {
+    return res.status(400).json({ message: `channel ${target} is not connected to this service`});
   }
 });
 
