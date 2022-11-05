eb06ef73fb7f59e061b8acf2d07fa2eda3f65bf6
Fynises <fraser_irvine@outlook.com>
2022-11-05 22:13:40 +0000

    adding more environment variables

--- config/config.ts
+++ config/config.ts
@@ -1,11 +1,18 @@
+/**
+ * production setting to true will authorise the app programmatically, don't use for testing
+ */
+
 const config = {
   port: process.env.PORT,
   wsPort: process.env.WS_PORT,
   wsUrl: process.env.WS_URL,
+  production: process.env.PRODUCTION || false,
   twitchChatUrl: process.env.TWITCH_CHAT_URL,
   twitchClientId: process.env.TWITCH_CLIENT_ID,
-  twitchAccessToken: process.env.TWITCH_ACCESS_TOKEN,
-  twitchOAuth: process.env.TWITCH_OAUTH
+  twitchClientSecret: process.env.TWITCH_CLIENT_SECRET,
+  twitchAccessToken: process.env.TWITCH_ACCESS_TOKEN || '',
+  twitchOAuth: process.env.TWITCH_OAUTH || '',
+  twitchRefresh: ''
 };
 
 export default config;
