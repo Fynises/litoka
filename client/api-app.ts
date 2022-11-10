734a72c6deab704c970cc0141945037f6789859e
Fynises <fraser_irvine@outlook.com>
2022-11-10 21:20:12 +0000

    removed trailing slash from fetch

--- client/api-app.ts
+++ client/api-app.ts
@@ -1,6 +1,6 @@
 const fetchWsUrl = async (signal: AbortSignal) => {
   try {
-    const response = await fetch('/api/getwebsocket/', { method: 'GET', signal: signal });
+    const response = await fetch('/api/getwebsocket', { method: 'GET', signal: signal });
     return await response.json();
   } catch (err) {
     console.log(err);
