1951ca7995e9a5b7cddfc609aebddfa05bd7826d
Fynises <fraser_irvine@outlook.com>
2022-11-11 20:08:06 +0000

    changed to snake_case

--- server/src/routes/api_routes.rs
+++ server/src/routes/api_routes.rs
@@ -2,16 +2,15 @@ use actix_web::{web, get, Responder, Result};
 use serde::Serialize;
 use crate::lib;
 
-#[allow(non_snake_case)]
 #[derive(Serialize)]
 struct WebSocketUrl {
-    wsUrl: String,
+    ws_url: String,
 }
 
 #[get("/api/getwebsocket")]
 pub async fn get_web_socket() -> Result<impl Responder> {
     let res = WebSocketUrl {
-        wsUrl: lib::CONFIG.ws_url.to_string()
+        ws_url: lib::CONFIG.ws_url.to_string()
     };
     Ok(web::Json(res))
 }
