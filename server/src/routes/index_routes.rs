cd19569daa679abbeba21810bbecfbedf5a88612
Fynises <fraser_irvine@outlook.com>
2022-11-11 20:05:22 +0000

    get index path from config

--- server/src/routes/index_routes.rs
+++ server/src/routes/index_routes.rs
@@ -1,11 +1,11 @@
 use actix_web::routes;
 use actix_files::NamedFile;
 use std::io::Result;
-use std::path::Path;
+use crate::lib;
 
 #[routes]
 #[get("/")]
 #[get("/shoutout")]
-pub async fn index() -> Result<NamedFile>{
-    Ok(NamedFile::open(Path::new(env!("CARGO_MANIFEST_DIR")).join("../public/index.html"))?)
+pub async fn index() -> Result<NamedFile> {
+    Ok(NamedFile::open(lib::CONFIG.index_path.as_str()).unwrap())
 }
